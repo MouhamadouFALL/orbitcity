221bd737026cafdc81e9d09138bd38c3eeecf063
Mouhamadou Fall <mouhamadou2.fall@gmail.com>
2024-10-15 20:47:04 +0200

    purchase_order: Mise à jour de la méthode `button_confirm` pour associer la signature de l'utilisateur connecté
    - Ajout de la logique permettant d'enregistrer la signature de l'utilisateur qui confirme le bon d'achat

--- models/purchase_order.py
+++ models/purchase_order.py
@@ -7,3 +7,12 @@ class PurchaseOrder(models.Model):
 
     attachment_ids = fields.Many2many('ir.attachment', 'orbit_attachment_rel', 'orbit_id', 'attachment_id', string="Pieces jointes", store=True, help="Attach files related to this order")
 
+    usr_confirmed = fields.Many2one('res.users', string="Confirmé par", readonly=True)
+
+    def button_confirm(self):
+        """Overrides the confirm button method to record the user who confirmed."""
+        res = super(PurchaseOrder, self).button_confirm()
+        for order in self:
+            # Enregistre l'utilisateur connecté
+            order.usr_confirmed = self.env.user  
+        return res
